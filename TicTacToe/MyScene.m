6f37067990fb05078aa3a1450c9e64eaf2941917
francisco-dechoudens <francisco.dechoudens@gmail.com>
2014-05-30 15:50:17 -0400

    Add ending condition

--- TicTacToe/MyScene.m
+++ TicTacToe/MyScene.m
@@ -10,6 +10,8 @@
 
 @implementation MyScene{
     NSMutableArray *gridStatus;
+    NSMutableArray *playersTokens;
+    NSString *positionName,*name1,*name2,*name3;
     CGPoint locationToUIPosition;
     int player;
 }
@@ -24,9 +26,15 @@
         [self addChild:bg];
         
         gridStatus = [[NSMutableArray alloc]init];
+        playersTokens = [NSMutableArray new];
+        NSMutableArray *emptyToken = [NSMutableArray new];
+        
         for (int i = 0; i<9; i++) {
             [gridStatus addObject:@"1"];
+            [emptyToken addObject:@0];
         }
+        [playersTokens addObject:emptyToken];
+        [playersTokens addObject:emptyToken];
         
         player = 1; //initial player - turtle
     }
@@ -46,26 +54,127 @@
             if (player) {
                 sprite = [SKSpriteNode spriteNodeWithImageNamed:@"turtle"];
                 sprite.scale = 0.3;
+                sprite.position = locationToUIPosition;
+                sprite.name = positionName;
+                [self addChild:sprite];
+                if ([self checkWithCondition]) {
+                    self.userInteractionEnabled = NO;
+                    [self enumerateChildNodesWithName:name1 usingBlock:^(SKNode *node, BOOL *stop) {
+                        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+                        [node runAction:[SKAction repeatActionForever:action]];
+                        
+                    }];
+                    [self enumerateChildNodesWithName:name2 usingBlock:^(SKNode *node, BOOL *stop) {
+                        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+                        [node runAction:[SKAction repeatActionForever:action]];
+                        
+                    }];
+                    [self enumerateChildNodesWithName:name3 usingBlock:^(SKNode *node, BOOL *stop) {
+                        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+                        [node runAction:[SKAction repeatActionForever:action]];
+                        
+                    }];
+                }
                 player = 0;
             }
             else{
                 sprite = [SKSpriteNode spriteNodeWithImageNamed:@"rabbit"];
                 sprite.scale = 0.18;
+                sprite.position = locationToUIPosition;
+                sprite.name = positionName;
+                [self addChild:sprite];
+                if ([self checkWithCondition]) {
+                    self.userInteractionEnabled = NO;
+                    [self enumerateChildNodesWithName:name1 usingBlock:^(SKNode *node, BOOL *stop) {
+                        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+                        [node runAction:[SKAction repeatActionForever:action]];
+                        
+                    }];
+                    [self enumerateChildNodesWithName:name2 usingBlock:^(SKNode *node, BOOL *stop) {
+                        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+                        [node runAction:[SKAction repeatActionForever:action]];
+                        
+                    }];
+                    [self enumerateChildNodesWithName:name3 usingBlock:^(SKNode *node, BOOL *stop) {
+                        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+                        [node runAction:[SKAction repeatActionForever:action]];
+                        
+                    }];
+                }
                 player = 1;
             }
             
-            sprite.position = locationToUIPosition;
-            
-            //SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-            
-            //[sprite runAction:[SKAction repeatActionForever:action]];
-            
-            [self addChild:sprite];
         }
-       
-        
-        
     }
+}
+
+-(BOOL)checkWithCondition{
+   
+     NSMutableArray *temp =  [[NSMutableArray alloc]initWithArray:playersTokens[player]];
+    
+     //check#1 - horizontal
+    if ([temp[0]intValue]+[temp[1]intValue]+[temp[2]intValue] == 15) {
+        NSLog(@"win player %d",player);
+        name1 = @"0";
+        name2 = @"1";
+        name3 = @"2";
+        return true;
+    }
+    else if ([temp[3]intValue]+[temp[4]intValue]+[temp[5]intValue] == 15) {
+        NSLog(@"win player %d",player);
+        name1 = @"3";
+        name2 = @"4";
+        name3 = @"5";
+          return true;
+    }
+    else if ([temp[6]intValue]+[temp[7]intValue]+[temp[8]intValue] == 15) {
+        NSLog(@"win player %d",player);
+        name1 = @"6";
+        name2 = @"7";
+        name3 = @"8";
+          return true;
+    }
+    
+    //check#2 - vertical
+    else if ([temp[0]intValue]+[temp[3]intValue]+[temp[6]intValue] == 15) {
+         NSLog(@"win player %d",player);
+        name1 = @"0";
+        name2 = @"3";
+        name3 = @"6";
+          return true;
+    }
+    else if ([temp[1]intValue]+[temp[4]intValue]+[temp[7]intValue] == 15) {
+         NSLog(@"win player %d",player);
+        name1 = @"1";
+        name2 = @"4";
+        name3 = @"7";
+          return true;
+    }
+    else if ([temp[2]intValue]+[temp[5]intValue]+[temp[8]intValue] == 15) {
+         NSLog(@"win player %d",player);
+        name1 = @"2";
+        name2 = @"5";
+        name3 = @"8";
+          return true;
+    }
+    
+    //check#3 - diagonal
+    else if ([temp[0]intValue]+[temp[4]intValue]+[temp[8]intValue] == 15) {
+        NSLog(@"win player %d",player);
+        
+        name1 = @"0";
+        name2 = @"4";
+        name3 = @"8";
+          return true;
+    }
+    else if ([temp[2]intValue]+[temp[4]intValue]+[temp[6]intValue] == 15) {
+        NSLog(@"win player %d",player);
+        name1 = @"2";
+        name2 = @"4";
+        name3 = @"6";
+          return true;
+    }
+    return false;
 }
 
 -(BOOL)checkIfValid:(CGPoint) location{
@@ -77,11 +186,14 @@
     //location.x>0 && location.x<100 first column
     //location.x>110 && location.x<210 second column
     //location.x>220 && location.x<320 third column
-    
+    NSMutableArray *temp =  [[NSMutableArray alloc]initWithArray:playersTokens[player]];
     if (location.x>0 && location.x<100 && location.y>320) {
         if ([gridStatus[0] boolValue]) {
         locationToUIPosition = CGPointMake(0+100/2, (320+460)/2);
          gridStatus[0] = @"0";
+         positionName  = @"0";
+         [temp replaceObjectAtIndex:0 withObject:@4];
+         playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -89,6 +201,9 @@
         if ([gridStatus[1] boolValue]) {
             locationToUIPosition = CGPointMake((110+210)/2, (320+460)/2);
             gridStatus[1] = @"0";
+            positionName  = @"1";
+            [temp replaceObjectAtIndex:1 withObject:@9];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -96,6 +211,9 @@
         if ([gridStatus[2] boolValue]) {
             locationToUIPosition = CGPointMake((220+320)/2, (320+460)/2);
             gridStatus[2] = @"0";
+            positionName  = @"2";
+            [temp replaceObjectAtIndex:2 withObject:@2];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -103,6 +221,9 @@
         if ([gridStatus[3] boolValue]) {
             locationToUIPosition = CGPointMake((100+0)/2, (170+300)/2);
             gridStatus[3] = @"0";
+            positionName  = @"3";
+            [temp replaceObjectAtIndex:3 withObject:@3];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -110,6 +231,9 @@
         if ([gridStatus[4] boolValue]) {
             locationToUIPosition = CGPointMake((110+210)/2, (170+300)/2);
             gridStatus[4] = @"0";
+            positionName  = @"4";
+            [temp replaceObjectAtIndex:4 withObject:@5];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -117,6 +241,9 @@
         if ([gridStatus[5] boolValue]) {
             locationToUIPosition = CGPointMake((220+320)/2, (170+300)/2);
             gridStatus[5] = @"0";
+            positionName  = @"5";
+            [temp replaceObjectAtIndex:5 withObject:@7];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -124,6 +251,9 @@
         if ([gridStatus[6] boolValue]) {
             locationToUIPosition = CGPointMake((0+100)/2, (0+150)/2);
             gridStatus[6] = @"0";
+            positionName  = @"6";
+            [temp replaceObjectAtIndex:6 withObject:@8];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -131,6 +261,9 @@
         if ([gridStatus[7] boolValue]) {
             locationToUIPosition = CGPointMake((110+210)/2, (0+150)/2);
             gridStatus[7] = @"0";
+            positionName  = @"7";
+            [temp replaceObjectAtIndex:7 withObject:@1];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
@@ -138,6 +271,9 @@
         if ([gridStatus[8] boolValue]) {
             locationToUIPosition = CGPointMake((220+320)/2, (0+150)/2);
             gridStatus[8] = @"0";
+            positionName  = @"8";
+            [temp replaceObjectAtIndex:8 withObject:@6];
+            playersTokens[player] = temp;
             return TRUE;
         }
     }
